b592a0524df584beede137ba5b5bf6dbff11cbd0
Ollivier Robert <roberto@keltia.net>
2021-10-22 01:53:52 +0200

    Create an atlas_rs::version() method.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,28 @@
+use clap::{crate_name, crate_version};
+
 pub mod client;
 pub mod common;
+pub mod probes;
+
+/// Basic version string
+///
+/// Examples:
+/// ```
+/// println!("{}", atlas_rs::version());
+/// ```
+///
+pub fn version() -> String {
+    format!("{}/{}", crate_name!(), crate_version!())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_version() {
+        let v = version();
+
+        assert_eq!(format!("{}/{}", crate_name!(), crate_version!()), v);
+    }
+}
