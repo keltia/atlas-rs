3157eb39449100746225ed7d6813dc4d44487861
Ollivier Robert <roberto@keltia.net>
2021-11-03 22:40:55 +0100

    Itertools to the rescue.

--- src/common.rs
+++ src/common.rs
@@ -7,14 +7,18 @@ use std::collections::HashMap;
 /// Our crates
 use crate::client::Client;
 
+/// External crates
+use itertools::Itertools;
+
 impl<'cl> Client<'cl> {}
 
 /// Take an url and a set of options to add to the parameters
 pub fn add_opts<'cl>(url: &String, opts: HashMap<&'cl str, &'cl str>) -> String {
     let full = url.clone() + "?";
     let mut v = Vec::<String>::new();
-    for (name, val) in opts.into_iter() {
-        let opt = format!("{}={}", name, val);
+
+    for name in opts.keys().sorted() {
+        let opt = format!("{}={}", name, opts[name]);
         v.push(opt);
     }
     full + &v.join("&")
@@ -31,6 +35,6 @@ mod tests {
         let o = HashMap::from([("name", "foo"), ("bar", "baz")]);
 
         let url = add_opts(&url, o);
-        assert_eq!("/hello?name=foo&bar=baz", url);
+        assert_eq!("/hello?bar=baz&name=foo", url);
     }
 }
