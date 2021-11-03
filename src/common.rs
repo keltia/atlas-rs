11d184a1804e66d3cf1b1930b26a621b204fa4a7
Ollivier Robert <roberto@keltia.net>
2021-11-03 01:20:52 +0100

    Add add_opts() to merge options in the URL.

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,37 @@
+//! Some commonly used functions
+//!
+
+/// Standard library
+use std::collections::HashMap;
+
+/// Our crates
 use crate::client::Client;
+use crate::errors::APIError;
 
 impl<'cl> Client<'cl> {}
+
+/// Take an url and a set of options to add to the parameters
+pub fn add_opts<'cl>(url: &String, opts: HashMap<&'cl str, &'cl str>) -> String {
+    let full = url.clone() + "?";
+    let mut v = Vec::<String>::new();
+    for (name, val) in opts.into_iter() {
+        let opt = format!("{}={}", name, val);
+        v.push(opt);
+    }
+    full + &v.join("&")
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::common::add_opts;
+
+    #[test]
+    fn test_add_opts() {
+        let url = "/hello".to_string();
+        let o = HashMap::from([("name","foo"), ("bar", "baz")]);
+
+        let url = add_opts(&url, o);
+        assert_eq!("/hello?name=foo&bar=baz", url);
+    }
+}
