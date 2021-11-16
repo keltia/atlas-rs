1927aecb66f337b29a769d56eb2f6a162e79adcb
Ollivier Robert <roberto@keltia.net>
2021-11-16 17:29:24 +0100

    WIP: Major rewrite/redesign of the API surface (see APIDESIGN.md).
    - Add ClientBuilder struct and move most methods from Client
    - Client is just plain defaults and methods for providing context for later calls
    - Rewrite tests

--- src/client.rs
+++ src/client.rs
@@ -1,16 +1,102 @@
-//! Code to create `Client` instance which is the main container for the API
+//! Module to create a `Client` instance which is the main container for the API
+//! This is the main client struct and the different configuration methods.
+//!
+//! The way to configure it is different from the Go way and more in line
+//! with current Rust practices.
+//!
+//! The only mandatory argument is the API key so it is given to `new()` and
+//! all the other methods are there for configuration everything you want to
+//! change from the default.
+//!
+//! `NOTE` none of the fields are public except within the crate
+//!
+//! There are several classes of calls in the API:
+//!
+//! - entities
+//! - protocols
+//! - utility functions
+//!
+//! Each class is selected by a method on the `Client` struct such as `probe()` or `measurement()`.
+//! Calling one of these methods sets up the context for further calls with `RequestBuilder`
+//! (or plain `Request`).
+//!
+//! Examples:
+//! ```no_run
+//! use atlas_rs::client::{AF, ClientBuilder};
+//!
+//! let c = ClientBuilder::new()
+//!             .api_key("FOO")
+//!             .onoff(true)
+//!             .default_probe(666)
+//!             .want_af(AF::V4).build();
+//!
+//! let r = c.probe().get(666).call()?;
+//! ```
+//!
+//! ```no_run
+//! use atlas_rs::client::{AF, ClientBuilder};
+//!
+//! let c = ClientBuilder::new()
+//!             .api_key("FOO")
+//!             .onoff(true)
+//!             .default_probe(666)
+//!             .want_af(AF::V4)
+//!             .build();
+//!
+//! let r = c.traceroute().to("next.example.net").call()?;
+//! ```
+//!
+//! or
+//!
+//! ```no_run
+//! use atlas_rs::client::Client;
+//!
+//! let c = Client::new();
+//!
+//! let r = c.traceroute().to("next.example.net").call()?;
+//! ```
+//!
+//! like
+//!
+//! ```no_run
+//! use atlas_rs::client::Client;
+//!
+//! let c = Client::new();
+//!
+//! let r = c.ntp().from("next.example.net").call()?;
+//! ```
+//!
+//! (do we want a Client::NTP(next.example.net") or not?)
+//!
+//! Errors are handled in two steps:
+//! 1. if there is a Transport error (Unknown Host, Unreachable, etc.) call() will return an error
+//! 2. if the API returns an error, we attempt to decode as an APIError. If not, everything is good.
+//!
+//! So every `call()` returns a `Result<something, APIError>`.
+//!
+//! We use [reqwest] as HTTP client.  It has support for everything we need incl. proxy.  We choose
+//! to use the blocking client as most of the time this ought to be enough and it is easier.
+//!
+//! [reqwest]: https://crates.io/reqwest/
 //!
 
-/// Standard library
+// Standard library
 use std::collections::HashMap;
 use std::time::Duration;
 
-/// External crates
+// External crates
+use anyhow::{anyhow, Result};
 use clap::{crate_name, crate_version};
-use reqwest::blocking::ClientBuilder;
+
+// Internal crates
+use crate::request::RequestBuilder;
+
+// ---------------------------------------------------------------------------
 
 /// We target the v2 API (not sure if it needs to be public)
 pub(crate) const ENDPOINT: &str = "https://atlas.ripe.net/api/v2";
+
+// ---------------------------------------------------------------------------
 
 /// Represents all possible INET Address Family values
 #[derive(Debug, PartialEq)]
@@ -23,43 +109,22 @@ pub enum AF {
     V46,
 }
 
-/// This is the main client struct and the different configuration methods.
+// ---------------------------------------------------------------------------
+
+/// This is the main `Client` struct.  It holds all the parameters and the HTTP client handle.
+/// When using `Client::new()`, you get all the defaults values, if you want to configure it,
+/// please use `ClientBuilder` instead.
 ///
-/// The way to configure it is different from the Go way and more in line
-/// with current Rust practices.
+/// Example:
+/// ```no_run
+/// # use atlas_rs::client::Client;
 ///
-/// The only mandatory argument is the API key so it is given to `new()` and
-/// all the other methods are there for configuration everything you want to
-/// change from the default.
-///
-/// `NOTE` none of the fields are public except within the crate
-///
-/// `XXX` There is no `api_key()` method to enable changing the API key between
-/// calls.  Not sure it would be useful.
-///
-/// Examples:
+/// let c = Client::new();
 /// ```
-/// use atlas_rs::client::{AF,Client};
-///
-/// let c = Client::new("FOOBAR")
-///             .onoff(true)
-///             .default_probe(666)
-///             .want_af(AF::V4);
-/// ```
-///
-/// Errors are handled in two steps:
-/// 1. if there is a Transport error (Unknown Host, Unreachable, etc.) call() will return an error
-/// 2. if the API returns an error, we attempt to decode as an APIError. If not, everything is good.
-///
-/// We use [reqwest] as HTTP client.  It has support for everything we need incl. proxy.  We choose
-/// to use the blocking client as most of the time this ought to be enough and it is easier.
-///
-/// [reqwest]: https://crates.io/reqwest/
-///
 #[derive(Debug)]
 pub struct Client<'cl> {
     /// Mandatory
-    pub(crate) api_key: &'cl str,
+    pub(crate) api_key: Option<&'cl str>,
 
     /// Optional
     pub(crate) endpoint: &'cl str,
@@ -79,12 +144,35 @@ pub struct Client<'cl> {
     pub(crate) agent: Option<reqwest::blocking::Client>,
 }
 
-/// Default values
+/// Default values for Client
+/// 
 impl<'cl> Default for Client<'cl> {
     /// Defines all the default values
     fn default() -> Self {
+        Client::new()
+    }
+}
+
+/// All methods for `Client` for configuration
+/// 
+impl<'cl> Client<'cl> {
+    // ---------------------------------------------------------------------
+    // Public API
+    
+    /// Creates a bare client with defaults except for the API key which limits to certain
+    /// RIPE Atlas calls.
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::Client;
+    ///
+    /// let c = Client::new();
+    /// ```
+    ///
+    pub fn new() -> Client<'cl> {
         Client {
-            api_key: "<CHANGEME>",
+            api_key: None,
             endpoint: ENDPOINT,
             default_probe: 0,
             area_type: "area",
@@ -96,212 +184,83 @@ impl<'cl> Default for Client<'cl> {
             tags: "",
             opts: HashMap::new(),
             agent: None,
-        }
-    }
-}
-
-/// All methods for `Client` for configuration
-impl<'cl> Client<'cl> {
-    /// Create a new `Client` instance with the specified key
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// let c = Client::new("FOO");
-    /// ```
-    ///
-    pub fn new<S: Into<&'cl str>>(key: S) -> Self {
-        let mut c = Client {
-            api_key: key.into(),
-            ..Default::default()
-        }
-        .httpclient();
-        c.opts.insert("key", c.api_key);
-        c
+        }.httpclient()
     }
 
-    /// Sets the API endpoint
+    /// Create a ClientBuilder struct and returns it for chained calls
     ///
-    /// Examples
+    /// Example:
     ///
     /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .endpoint("https://example.com/v1")
-    /// # ;
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::builder();
     /// ```
     ///
-    pub fn endpoint<S: Into<&'cl str>>(mut self, v: S) -> Self {
-        self.endpoint = v.into();
-        self
+    pub fn builder() -> ClientBuilder<'cl> {
+        ClientBuilder::new()
     }
 
-    /// Sets the default probe ID
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .default_probe(666)
-    /// # ;
-    /// ```
-    ///
-    pub fn default_probe(mut self, v: u32) -> Self {
-        self.default_probe = v;
-        self
+    // ---------------------------------------------------------------------
+    // Entities
+    
+    pub fn credits(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Limits the scope by specifying an area type
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .area_type("area")
-    /// # ;
-    /// ```
-    ///
-    pub fn area_type<S: Into<&'cl str>>(mut self, v: S) -> Self {
-        self.area_type = v.into();
-        self
+    pub fn keys(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Limits the scope to this particular area value
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .area_value("WW")
-    /// # ;
-    /// ```
-    ///
-    pub fn area_value<S: Into<&'cl str>>(mut self, v: S) -> Self {
-        self.area_value = v.into();
-        self
+    pub fn measurement(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Sets the one-shot flag
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .onoff(true)
-    /// # ;
-    /// ```
-    ///
-    pub fn onoff(mut self, v: bool) -> Self {
-        self.is_oneoff = v;
-        self
+    pub fn probe(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Sets the pool size
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .pool_size(20)
-    /// # ;
-    /// ```
-    ///
-    pub fn pool_size(mut self, v: usize) -> Self {
-        self.pool_size = v;
-        self
+    // ---------------------------------------------------------------------
+    // Protocols
+
+    pub fn dns(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Sets the verbose flag
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::Client;
-    /// Client::new("FOO")
-    ///     .verbose(true)
-    /// # ;
-    /// ```
-    ///
-    pub fn verbose(mut self, v: bool) -> Self {
-        self.verbose = v;
-        self
+    pub fn http(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Sets the inet family, either v4 or v6 or both.
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::{AF,Client};
-    /// Client::new("FOO")
-    ///     .want_af(AF::V6)
-    /// # ;
-    /// ```
-    ///
-    pub fn want_af(mut self, v: AF) -> Self {
-        self.want_af = v;
-        self
+    pub fn ntp(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Sets the tags to be sent with the requests
-    ///    +tag / tag  ==> tags_include
-    ///    -tag / !tag ==> tags_exclude
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use atlas_rs::client::{AF,Client};
-    /// Client::new("FOO")
-    ///     .tags("ftth !cable")
-    /// # ;
-    /// ```
-    ///
-    pub fn tags<S: Into<&'cl str>>(mut self, v: S) -> Self {
-        self.tags = v.into();
-        self
+    pub fn ping(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Add options
-    ///
-    /// Examples
-    ///
-    /// ```no_run
-    /// # use std::collections::HashMap;
-    /// use atlas_rs::client::{AF,Client};
-    ///
-    /// Client::new("FOO")
-    ///     .opts(&HashMap::from([
-    ///                        ("is_anchor", "true")
-    ///                    ]))
-    /// # ;
-    /// ```
-    ///
-    pub fn opts(mut self, opts: &HashMap<&'cl str, &'cl str>) -> Self {
-        for (k, v) in opts.iter() {
-            self.opts.insert(k, v);
-        }
-        self
+    pub fn tlscert(&self) -> RequestBuilder {
+        unimplemented!()
     }
 
-    /// Create an instance of the HTTP client and attach it there
+    pub fn traceroute(&self) -> RequestBuilder {
+        unimplemented!()
+    }
+
+    // ---------------------------------------------------------------------
+    // Helpers/shortcuts
+
+    // *placeholder*
+
+    // ---------------------------------------------------------------------
+    // Private functions
+
+    /// Create an instance of the HTTP client and attach it there.  It is called as part of
+    /// .build().
     ///
-    /// Examples
-    /// ```no_run
-    /// # use atlas_rs::client::{AF,Client};
-    /// Client::new("FOO")
-    ///     .httpclient()
-    /// # ;
-    /// ```
-    ///
-    pub fn httpclient(mut self) -> Self {
+    fn httpclient(mut self) -> Self {
         let ag = format!("{}/{}", crate_name!(), crate_version!());
-        let agent = ClientBuilder::new()
+        let agent = reqwest::blocking::ClientBuilder::new()
             .connect_timeout(Duration::from_secs(10))
             .timeout(Duration::from_secs(5))
             .user_agent(&ag)
@@ -312,12 +271,253 @@ impl<'cl> Client<'cl> {
     }
 }
 
+// ---------------------------------------------------------------------------
+
+/// `ClientBuilder` is the main struct to create and configure a `Client`
+///
+pub struct ClientBuilder<'cl> {
+    cl: Client<'cl>,
+}
+
+/// Default values for `ClientBuilder`
+///
+impl<'cl> Default for ClientBuilder<'cl> {
+    /// Defines all the default values
+    fn default() -> Self {
+        ClientBuilder::new()
+    }
+}
+
+/// Methods for `ClientBuilder`
+///
+impl<'cl> ClientBuilder<'cl> {
+    // ---------------------------------------------------------------------
+    // Public API
+    
+    /// Create a new `ClientBuilder` instance
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    /// let c = ClientBuilder::new();
+    /// ```
+    ///
+    pub fn new() -> Self {
+        ClientBuilder { cl: Client::new() }
+    }
+
+    /// Create the final Client after checking the API key has been changed
+    ///
+    pub fn build(self) -> Result<Client<'cl>> {
+        match self.cl.api_key {
+            None => Err(anyhow!("You must change the default key")),
+            Some(_k) => {
+                Ok(self.cl)
+            },
+        }
+    }
+
+    /// Set the API key
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///         .api_key("FOO");
+    /// ```
+    ///
+    pub fn api_key<S: Into<&'cl str>>(mut self, key: S) -> Self {
+        self.cl.api_key = Some(key.into());
+        self
+    }
+
+    /// Sets the API endpoint
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .endpoint("https://example.com/v1")
+    /// # ;
+    /// ```
+    ///
+    pub fn endpoint<S: Into<&'cl str>>(mut self, v: S) -> Self {
+        self.cl.endpoint = v.into();
+        self
+    }
+
+    /// Sets the default probe ID
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .default_probe(666)
+    /// # ;
+    /// ```
+    ///
+    pub fn default_probe(mut self, v: u32) -> Self {
+        self.cl.default_probe = v;
+        self
+    }
+
+    /// Limits the scope by specifying an area type
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .area_type("area")
+    /// # ;
+    /// ```
+    ///
+    pub fn area_type<S: Into<&'cl str>>(mut self, v: S) -> Self {
+        self.cl.area_type = v.into();
+        self
+    }
+
+    /// Limits the scope to this particular area value
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .area_value("WW")
+    /// # ;
+    /// ```
+    ///
+    pub fn area_value<S: Into<&'cl str>>(mut self, v: S) -> Self {
+        self.cl.area_value = v.into();
+        self
+    }
+
+    /// Sets the one-shot flag
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .onoff(true)
+    /// # ;
+    /// ```
+    ///
+    pub fn onoff(mut self, v: bool) -> Self {
+        self.cl.is_oneoff = v;
+        self
+    }
+
+    /// Sets the pool size
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .pool_size(20)
+    /// # ;
+    /// ```
+    ///
+    pub fn pool_size(mut self, v: usize) -> Self {
+        self.cl.pool_size = v;
+        self
+    }
+
+    /// Sets the verbose flag
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .verbose(true)
+    /// # ;
+    /// ```
+    ///
+    pub fn verbose(mut self, v: bool) -> Self {
+        self.cl.verbose = v;
+        self
+    }
+
+    /// Sets the inet family, either v4 or v6 or both.
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .want_af(AF::V6)
+    /// # ;
+    /// ```
+    ///
+    pub fn want_af(mut self, v: AF) -> Self {
+        self.cl.want_af = v;
+        self
+    }
+
+    /// Sets the tags to be sent with the requests
+    ///    +tag / tag  ==> tags_include
+    ///    -tag / !tag ==> tags_exclude
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .tags("ftth !cable")
+    /// # ;
+    /// ```
+    ///
+    pub fn tags<S: Into<&'cl str>>(mut self, v: S) -> Self {
+        self.cl.tags = v.into();
+        self
+    }
+
+    /// Add options
+    ///
+    /// Example:
+    ///
+    /// ```no_run
+    /// # use std::collections::HashMap;
+    /// # use atlas_rs::client::ClientBuilder;
+    ///
+    /// let c = ClientBuilder::new()
+    ///     .opts(&HashMap::from([
+    ///                        ("is_anchor", "true")
+    ///                    ]))
+    /// # ;
+    /// ```
+    ///
+    pub fn opts(mut self, opts: &HashMap<&'cl str, &'cl str>) -> Self {
+        for (k, v) in opts.iter() {
+            self.cl.opts.insert(k, v);
+        }
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
+    fn test_client_new() {
         let c = Client::new("FOO");
 
         // Check all defaults
@@ -336,15 +536,37 @@ mod tests {
     }
 
     #[test]
+    fn test_clientbuilder_new() {
+        let cb = ClientBuilder::new().api_key("key").build();
+
+        assert!(cb.is_ok());
+        
+        let c = c.unwrap();
+        // Check all defaults
+        assert_eq!("key", c.cl.api_key);
+        assert_eq!(ENDPOINT, c.cl.endpoint);
+        assert_eq!(0, c.cl.default_probe);
+        assert_eq!("area", c.cl.area_type);
+        assert_eq!("WW", c.cl.area_value);
+        assert!(c.cl.is_oneoff);
+        assert_eq!(10, c.cl.pool_size);
+        assert_eq!(AF::V46, c.cl.want_af);
+        assert!(!c.cl.verbose);
+        assert_eq!("", c.cl.tags);
+        assert!(c.cl.opts.contains_key("key"));
+        assert!(c.cl.agent.is_some());
+    }
+
+    #[test]
     fn test_opts() {
         let h = HashMap::from([("foo", "a"), ("bar", "b"), ("key", "FOO")]);
-        let c = Client::new("FOO").opts(&h);
+        let c = ClientBuilder::new().opts(&h).build().unwrap();
         assert_eq!(h, c.opts);
     }
 
     #[test]
     fn test_onoff() {
-        let c = Client::new("FOO").onoff(true);
+        let c = ClientBuilder::new().onoff(true).build();
 
         assert!(c.is_oneoff);
     }
