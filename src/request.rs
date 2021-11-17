82bc02246cc4ebf0f3a5a02f2533f2cff14d2b51
Ollivier Robert <roberto@keltia.net>
2021-11-17 19:51:54 +0100

    WIP: RequestBuilder on the model of reqwest.

--- src/request.rs
+++ src/request.rs
@@ -1,8 +1,28 @@
-use reqwest::Request;
-
+use anyhow::{bail, Result};
 use crate::client::Client;
 
 pub struct RequestBuilder<'rq> {
-    pub c: Client<'rq>,
-    pub r: Request,
+    /// Context is which part of the API we are targetting (`/probe/`, etc.)
+    pub ctx: &'rq str,
+    pub c: &'rq Client<'rq>,
+    pub r: Result<reqwest::blocking::Request>,
 }
+
+impl<'rq> RequestBuilder<'rq> {
+    pub fn new(ctx: &'rq str, c: &'rq Client<'rq>, r: Result<reqwest::blocking::Request>) -> Self {
+        RequestBuilder {ctx, c, r}
+    }
+
+    pub fn call(self) -> Result<reqwest::Response> {
+        let r = match self.r {
+            Ok(r) => r,
+            Err(e) => bail!("e"),
+        };
+        let resp = r.get(r.method()).send();
+        resp
+    }
+}
+
+pub struct Request {
+}
+
