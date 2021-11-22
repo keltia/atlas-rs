fe9f902fcc3fed6af587f88bc0ba034414f25165
Ollivier Robert <roberto@keltia.net>
2021-11-22 17:40:05 +0100

    get() must dispatch to the right "type". Cmd is the new context. call() does the real call.

--- src/request.rs
+++ src/request.rs
@@ -39,24 +39,56 @@ impl<'a> From<Param<'a>> for u32 {
 #[derive(Debug)]
 pub struct RequestBuilder<'rq> {
     /// Context is which part of the API we are targetting (`/probe/`, etc.)
-    pub ctx: &'rq str,
+    pub ctx: Cmd,
     pub c: &'rq Client<'rq>,
     pub r: Result<reqwest::blocking::Request>,
 }
 
 impl<'rq> RequestBuilder<'rq> {
-    pub fn new(ctx: &'rq str, c: &'rq Client<'rq>, r: Result<reqwest::blocking::Request>) -> Self {
+    pub fn new(ctx: Cmd, c: &'rq Client<'rq>, r: Result<reqwest::blocking::Request>) -> Self {
         RequestBuilder {ctx, c, r}
     }
 
-    pub fn call(self) -> Result<reqwest::blocking::Response> {
+    pub fn get(self, data: Param<'rq>) -> Self
+    {
+        // Main routing
+        match self.ctx {
+            Cmd::Probes => {
+                return Probe::dispatch(self, probes::Ops::Get, data);
+            },
+            Cmd::Measurements => unimplemented!(),
+            Cmd::AnchorMeasurements => unimplemented!(),
+            Cmd::Credits => unimplemented!(),
+            Cmd::Anchors => unimplemented!(),
+            Cmd::Keys => unimplemented!(),
+            Cmd::ParticipationRequests => unimplemented!(),
+        }
+    }
+
+    pub fn call<T>(self) -> Result<T, APIError>
+        where T: de::Deserialize<'rq>
+    {
         let r = match self.r {
             Ok(r) => r,
-            Err(e) => bail!("e"),
+            Err(e) => return Err(APIError::new(500,
+                                               "bad request",
+                                               e.to_string().as_ref(),
+                                               "")),
         };
-        let resp = self.c.agent.as_ref().unwrap().clone()
+
+        let resp = self.c.agent.as_ref().unwrap()
             .get(r.url().as_str()).send()?;
-        Ok(resp)
+
+        let txt = match resp.text() {
+            Ok(t) => t,
+            Err(e) => return Err(APIError::new(500,
+                                               "no body",
+                                               e.to_string().as_ref(),
+                                               ""))
+        }.as_str();
+
+        let r: T = serde_json::from_str(txt)?;
+        Ok(r)
     }
 }
 
