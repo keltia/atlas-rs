1dafc31d6ec28dbe63bcfac8ef5a8d0abcc710cd
Ollivier Robert <roberto@keltia.net>
2021-12-07 11:01:56 +0100

    Properly document the example.

--- examples/call.rs
+++ examples/call.rs
@@ -1,24 +1,40 @@
+//! Example on how having a `Callable` trait should help the chained calls and
+//! return values/type.
+//!
+//! Thanks to kangalioo#9108 on Discord for it.
+//!
+
 use std::fmt::Debug;
 use anyhow::Result;
 
+/// Generic return type
+///
 #[derive(Debug)]
 struct Callable<T> { res: T }
 
+/// Implementation.
+///
 impl<T> Callable<T> {
+    /// This is the generic return type call
+    ///
     pub fn call(self) -> T
         where T: Debug,
     {
-
-        //println!("res={:?}", self.res)
-        unimplemented!()
+        println!("res={:?}", self.res);
+        self.res
+        //unimplemented!()
     }
 }
 
+/// This will be our RequestBuilder thingy
+/// 
 #[derive(Debug, Copy, Clone)]
 struct Request<T> {
     val: T,
 }
 
+/// And how we implement both `get()` and `list()`
+///
 impl<T> Request<T> {
     fn new(v: T) -> Self {
         Self { val: v }
@@ -35,21 +51,16 @@ impl<T> Request<T> {
     {
         Callable { res: vec![self.val, self.val ] }
     }
-
 }
 
-
+/// Practical example.
+///
 fn main() -> Result<()> {
     let a = Request::new(5);
     let b = Request::new("aa");
 
-    println!("{:?}", a.get());
-    println!("{:?}", a.list());
-
-    println!("{:?}", b.get());
-    println!("{:?}", b.list());
-
-    println!("{:?}", a.get().call());
+    let r = a.get().call();
+    let s = b.list().call();
 
     Ok(())
 }
