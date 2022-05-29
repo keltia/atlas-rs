e7b43f0e6bc1a43472f795fda34f2ff969d9ca57
Ollivier Robert <roberto@keltia.net>
2022-05-29 17:41:35 +0200

    Move add_opts to a more logical place.

--- src/request.rs
+++ src/request.rs
@@ -394,12 +394,41 @@ impl<'rq> RequestBuilder<'rq> {
         self
     }
 
-    /// Finalize the chain and call the real API
-    ///
-    pub fn call<T>(&self) -> Result<T, APIError>
-    where
-        T: de::DeserializeOwned + std::fmt::Display,
-    {
-        unimplemented!()
+/// Take an url and a set of options to add to the parameters
+///
+/// Example!
+/// ```no_run
+/// # use atlas_rs::option::{add_opts, Options};
+/// # use atlas_rs::request::add_opts;
+///
+/// let url = "https://example.com/";
+/// let opts = Options::from([("foo", "bar")]);
+/// let url = add_opts(&url, &opts);
+/// ```
+///
+pub fn add_opts(url: &str, opts: &Options) -> String {
+    let full = url.to_owned() + "?";
+    let mut v = Vec::<String>::new();
+
+    for name in opts.keys().sorted() {
+        let opt = format!("{}={}", name, opts[name]);
+        v.push(opt);
+    }
+    full + &v.join("&")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::option::Options;
+
+    use super::*;
+
+    #[test]
+    fn test_add_opts() {
+        let url = "/hello".to_string();
+        let o = Options::from([("name", "foo"), ("bar", "baz")]);
+
+        let url = add_opts(&url, &o);
+        assert_eq!("/hello?bar=baz&name=foo", url);
     }
 }
