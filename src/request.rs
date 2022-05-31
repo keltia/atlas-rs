f3c964d81be883fa1a9f224bfcc82da6e26af29d
Ollivier Robert <roberto@keltia.net>
2022-06-01 01:32:28 +0200

    Bring back real info(), need to change Op and stuff.

--- src/request.rs
+++ src/request.rs
@@ -378,9 +378,37 @@ impl RequestBuilder {
     ///
     pub fn info<T>(mut self) -> Result<T, APIError>
     where
-        T: de::DeserializeOwned + std::fmt::Display,
+        T: de::DeserializeOwned + Display,
     {
-        self.get(Param::None)
+        // Setup everything
+        //
+        let add = get_ops_url(&self.ctx, Op::Info, Param::None);
+        dbg!(&add);
+        let opts = self.c.opts.iter();
+
+        // Setup URL with potential parameters like `key`.
+        //
+        let url =
+            Url::parse_with_params(format!("{}{}", &self.r.url().as_str(), add).as_str(), opts)
+                .unwrap();
+
+        self.r = reqwest::blocking::Request::new(self.r.method().clone(), url);
+        let resp = self
+            .c
+            .agent
+            .as_ref()
+            .unwrap()
+            .get(self.r.url().as_str())
+            .send()?;
+
+        println!("{:?} - {:?}", self.c.opts, self.r.url().as_str());
+
+        let txt = resp.text()?;
+        println!("after text={}", txt);
+
+        let r: T = serde_json::from_str(&txt)?;
+        println!("after r={}", r);
+        Ok(r)
     }
 }
 
