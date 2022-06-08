b6a9eb9e2c76941457a14277b9e63a4e520dcfbc
Ollivier Robert <roberto@keltia.net>
2022-06-08 15:23:59 +0200

    No need for common, put it into mod.rs directly.

--- src/bin/atlas/cmds/mod.rs
+++ src/bin/atlas/cmds/mod.rs
@@ -1,6 +1,34 @@
-pub(crate) mod common;
+use clap::Parser;
+
 pub(crate) mod credits;
 pub(crate) mod ip;
 pub(crate) mod keys;
 pub(crate) mod measurements;
 pub(crate) mod probes;
+
+// These two struct are shared amongst the different commands/subcommands
+//
+
+/// Info options
+///
+#[derive(Parser)]
+pub(crate) struct InfoOpts {
+    /// Print debug info
+    #[clap(short)]
+    pub(crate) debug: bool,
+    /// Probe ID
+    pub(crate) id: Option<u32>,
+}
+
+/// List options
+///
+#[derive(Parser)]
+pub(crate) struct ListOpts {
+    /// Print debug info
+    #[clap(short)]
+    pub(crate) debug: bool,
+    /// Query parameters
+    #[clap(short)]
+    pub(crate) q: Vec<String>,
+}
+
