4f162b959ae4f50cc4f4b5322a2eaf6f8799022f
Ollivier Robert <roberto@keltia.net>
2022-11-04 00:32:51 +0100

    Add description.

--- atlas-api/src/core/mod.rs
+++ atlas-api/src/core/mod.rs
@@ -1,3 +1,9 @@
+//! This `core` component includes all the different parts of the API itself.
+//!
+//! There one module per major category which describe all the available types, implementation
+//! of the `Display` trait and the routing function `set_url()`.
+//!
+
 // main modules
 pub mod anchor_measurements;
 pub mod anchors;
